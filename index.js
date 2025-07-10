0e290b14d8cd515d2c9485e750b69d31cba8ce13
MohamedMedhat143 <120517976+MohamedMedhat143@users.noreply.github.com>
2025-07-10 23:05:27 +0300

    update the build front end code

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 import express from "express";
-import { dbConn } from "./database/dbconnection.js";
-import { AppError } from "./src/utils/appError.js";
-import { globalError } from "./src/middleware/globalError.js";
-import { bootstrap } from "./src/modules/bootstrap.js";
 import path from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 import cors from "cors";
+import { dbConn } from "./database/dbconnection.js";
+import { AppError } from "./src/utils/appError.js";
+import { globalError } from "./src/middleware/globalError.js";
+import { bootstrap } from "./src/modules/bootstrap.js";
+
 dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 4000;
+
 app.use(cors());
 app.use(express.static("Books"));
 app.use(express.json());
+
 bootstrap(app);
 
 // 📦 FRONTEND SETUP
@@ -32,5 +36,7 @@ app.get(/.*/, (req, res) => {
 app.use((req, res, next) => {
   next(new AppError(`route not found ${req.originalUrl}`, 404));
 });
+
 app.use(globalError);
+
 app.listen(port, () => console.log(`Klo Tmam ala elport da👌👌 ${port}!`));
